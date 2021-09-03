af4b12a1de249a7557316dc126f0826014318aac
Hamed Movaghari <hmovaghari@gmail.com>
2021-09-04 00:15:33 +0430

    Add project files.

---
+++ Persian.Calendar.Library/Calendar.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persian.Calendar.Library
+{
+    public class Calendar
+    {
+        public DateTime DateTime { get; set; }
+
+        public Calendar(DateTime dateTime, int hijriAdjustment = 0)
+        {
+            DateTime = dateTime;
+            HijriAdjustment = hijriAdjustment;
+        }
+
+        public Calendar(int hijriAdjustment = 0)
+        {
+            DateTime = DateTime.Now;
+            HijriAdjustment = hijriAdjustment;
+        }
+
+        public static bool IsValidRegion
+        {
+            get => CalendarFunctions.IsValidRegion();
+        }
+
+        public int HijriAdjustment { get; set; }
+
+        public int GregorianYear
+        {
+            get => DateTime.Year;
+        }
+
+        public int PersianYear
+        {
+            get => CalendarFunctions.GetPersianYear(DateTime);
+        }
+
+        public int HijriYear
+        {
+            get => CalendarFunctions.GetHijriYear(DateTime, HijriAdjustment);
+        }
+
+        public int GregorianMonth
+        {
+            get => DateTime.Month;
+        }
+
+        public int PersianMonth
+        {
+            get => CalendarFunctions.GetPersianMonth(DateTime);
+        }
+
+        public int HijriMonth
+        {
+            get => CalendarFunctions.GetHijriMount(DateTime, HijriAdjustment);
+        }
+
+        public string GregorianMonthTiltePersian
+        {
+            get => CalendarFunctions.GetGregorianMonthTiltePersian(DateTime);
+        }
+
+
+        public string PersianMonthTitle
+        {
+            get => CalendarFunctions.GetPersianMonthTitle(DateTime);
+        }
+
+        public string HijriMonthTitle
+        {
+            get => CalendarFunctions.GetHijriMonthTitle(DateTime, HijriAdjustment);
+        }
+
+
+        public int GregorianDay
+        {
+            get => DateTime.Day;
+        }
+
+        public int PersianDay
+        {
+            get => CalendarFunctions.GetPersianDay(DateTime);
+        }
+
+        public int HijriDay
+        {
+            get => CalendarFunctions.GetHijriDay(DateTime, HijriAdjustment);
+        }
+
+        public int GregorianDayOfYear
+        {
+            get => DateTime.DayOfYear;
+        }
+
+        public int PersianDayOfYear
+        {
+            get => CalendarFunctions.GetPersianDayOfYear(DateTime);
+        }
+
+        public int HijriDayOfYear
+        {
+            get => CalendarFunctions.GetHijriDayOfYear(DateTime, HijriAdjustment);
+        }
+
+        public string PersianDayOfWeek
+        {
+            get => CalendarFunctions.GetPersianDayOfWeek(DateTime.DayOfWeek);
+        }
+
+        public string PersianEvents
+        {
+            get => "";
+        }
+
+    }
+}
---
+++ Persian.Calendar.Library/EXMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persian.Calendar.Library
+{
+    public static class EXMethods
+    {
+        public static string ToPersianNumber(this int number)
+        {
+            return CalendarFunctions.ToPersianNumber(number.ToString());
+        }
+
+        public static string ToPersianNumber(this long number)
+        {
+            return CalendarFunctions.ToPersianNumber(number.ToString());
+        }
+
+        public static string ToPersianNumber(this string number)
+        {
+            return CalendarFunctions.ToPersianNumber(number);
+        }
+
+        public static string ToPersianOrdinalNumber(this int number)
+        {
+            return CalendarFunctions.ToPersianOrdinalNumber(number);
+        }
+    }
+}
