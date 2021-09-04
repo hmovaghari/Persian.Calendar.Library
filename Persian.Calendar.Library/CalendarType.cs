e6d16db3536554abf240f264f853d272e6d1b4be
Hamed Movaghari <hmovaghari@gmail.com>
2021-09-04 21:18:32 +0430

    add monthView from Farsi.Library 2.7.0

---
+++ Persian.Calendar.Library/CalendarType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persian.Calendar.Library
+{
+    public enum CalendarType : byte
+    {
+        PersianCalendar = 1,
+        HijriCalendar = 2,
+        GregorianCalendar = 3
+    }
+}
